8a2fba57bd50692cb1b385cc79e1b64113593eb3
RGoldenberg <mostly.costly@gmail.com>
2018-09-15 14:35:43 +0300

    add Quote and QuoteGrid components

--- wonderful-quotes/src/App.vue
+++ wonderful-quotes/src/App.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="container">
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+
     export default {
-        
+        data() {
+            return {
+                quotes: ['Carthago Delenda Est'],
+                maxQuotes: 10
+            };
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
+        }
     }
 </script>
 
