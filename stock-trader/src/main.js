d38bd750e49736584378ff8fbf0c84129b864912
RGoldenberg <mostly.costly@gmail.com>
2018-09-16 19:23:02 +0300

    add vue-router & routes, header template

--- stock-trader/src/main.js
+++ stock-trader/src/main.js
@@ -1,7 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import App from './App.vue';
+import { routes } from './routes';
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: 'history',
+  routes
+});
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
-})
+});
