93747ea023df274d2d1ed14dba8ae6ec0f59d7fd
RGoldenberg <mostly.costly@gmail.com>
2018-09-16 20:01:49 +0300

    add stock display

--- stock-trader/src/App.vue
+++ stock-trader/src/App.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="container">
         <app-header></app-header>
-        <router-view></router-view>
+        <div class="row">
+            <div class="col-xs-12">
+                <router-view></router-view>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -16,5 +20,7 @@
 </script>
 
 <style>
-
+    body {
+        padding: 30px;
+    }
 </style>
--- stock-trader/src/components/stocks/Stocks.vue
+++ stock-trader/src/components/stocks/Stocks.vue
@@ -1,3 +1,28 @@
 <template>
-    <h1>The Stocks Component</h1>
+    <div>
+        <app-stock 
+            v-for="stock in stocks"
+            :stock="stock"
+            :key="stock.id"></app-stock>
+    </div>
 </template>
+
+<script>
+    import Stock from './Stock.vue';
+
+    export default {
+        data() {
+            return {
+                stocks: [
+                    {id: 1, name: 'Amazon', price: 250},
+                    {id: 2, name: 'Google', price: 200},
+                    {id: 3, name: 'Apple', price: 150},
+                    {id: 4, name: 'Microsoft', price: 100},
+                ]
+            };
+        },
+        components: {
+            appStock: Stock   
+        }
+    }
+</script>
