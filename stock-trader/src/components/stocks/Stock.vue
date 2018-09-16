93759662862bd4215489c2c5013494ddded3a2dd
RGoldenberg <mostly.costly@gmail.com>
2018-09-16 20:50:26 +0300

    add stock purchasing logic

--- stock-trader/src/components/stocks/Stock.vue
+++ stock-trader/src/components/stocks/Stock.vue
@@ -42,6 +42,7 @@ export default {
                 quantity: this.quantity
             };
             console.log(order);
+            this.$store.dispatch('buyStock', order);
             this.quantity = 0;
         }
     }
---
+++ stock-trader/src/store/modules/portfolio.js
@@ -0,0 +1,58 @@
+const state = {
+    funds: 100000,
+    stocks: []
+};
+
+const mutations = {
+    BUY_STOCK(state, {stockId, stockPrice, quantity}) {
+        const record = state.stocks.find(stock => stock.id === stockId);
+        if (record) {
+            record.quantity += quantity;
+        } else {
+            state.stocks.push({
+                id: stockId, 
+                quantity
+            });
+        }
+        state.funds -= stockPrice * quantity;
+    },
+    SELL_STOCK(state, {stockId, stockPrice, quantity}) {
+        const record = state.stocks.find(stock => stock.id === stockId);
+        if (record.quantity > quantity) {
+            record.quantity -= quantity;
+        } else {
+            state.stocks.splice(state.stocks.indexOf(record), 1);
+        }
+        state.funds += stockPrice * quantity;
+    }
+};
+
+const actions = {
+    sellStock({ commit }, order) {
+        commit('SELL_STOCK', order);
+    }
+};
+
+const getters = {
+    stockPortfolio(state) {
+        return state.stocks.map(stock => {
+            const record = getters.stocks.find(el => el.id === stock.id);
+            return {
+                id: stockId,
+                quantity: stock.quantity,
+                name: record.name,
+                price: record.price
+            };
+        });
+    },
+    funds(state) {
+        return state.funds;
+    }
+};
+
+export default {
+    state,
+    mutations,
+    actions,
+    getters
+};
