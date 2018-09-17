fc904f8dfd4af91e1e3ed9367fbbca521ee21311
RGoldenberg <mostly.costly@gmail.com>
2018-09-17 21:42:19 +0300

    edit homepage info text

--- stock-trader/src/components/Home.vue
+++ stock-trader/src/components/Home.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
         <h1>Trade or view your Portfolio</h1>
-        <h6>You may Save & Load your data</h6>
         <h6>Click on 'End Day' to begin a New Day!</h6>
         <hr>
         <p>Your Funds: {{ funds | currency }}</p>
